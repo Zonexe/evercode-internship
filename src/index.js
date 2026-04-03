d8ff388115ab069e2ebd7712dc1015bb6b763160
Zonexe <168166597+Zonexe@users.noreply.github.com>
2026-04-03 10:47:39 +0300

    feat: add scheduler module and periodic task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 const logger = require("./logger");
+const { startTask } = require('./scheduler');
 
 logger("Приложение успешно запущено!");
+
+startTask('MyPeriodicTask', 10000, () => {
+    logger('running');
+});
