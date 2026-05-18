fb2fbb5fa4fa3f2aa40523a458a1c5f4a8520edf
Zonexe <168166597+Zonexe@users.noreply.github.com>
2026-05-19 02:27:36 +0300

    refactor(di): update Scheduler import to named export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import container from "./container";
 import { myPeriodicTask } from "./tasks/myTask";
 import { ILogger } from "./utils/logger";
-import Scheduler from "./services/scheduler";
+import { Scheduler } from "./services/scheduler";
 
 const logger = container.resolve<ILogger>("logger");
 const scheduler = container.resolve<Scheduler>("scheduler");
@@ -9,6 +9,8 @@ const scheduler = container.resolve<Scheduler>("scheduler");
 import configType from "./config";
 const config = container.resolve<typeof configType>("config");
 
-logger.info(`Приложение версии ${config.version} успешно запущено на порту ${config.port}!`);
+logger.info(
+  `Приложение версии ${config.version} успешно запущено на порту ${config.port}!`,
+);
 
 scheduler.startTask("MyPeriodicTask", 10000, myPeriodicTask);
