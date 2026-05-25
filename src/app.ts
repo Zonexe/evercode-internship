1d035965c29723b5b7778da9e292054e7341988c
Zonexe <168166597+Zonexe@users.noreply.github.com>
2026-05-26 01:21:41 +0300

    feat(app): apply authMiddleware to protected routes selectively

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
-import express, { Application, Request, Response } from "express";
+import express, {
+  Application,
+  Request,
+  Response,
+  RequestHandler,
+} from "express";
 import { ILogger } from "./utils/logger";
 import AppConfig from "./config";
 
@@ -6,16 +11,20 @@ export class App {
   public readonly expressApp: Application;
   private readonly logger: ILogger;
   private readonly config: typeof AppConfig;
+  private readonly authMiddleware: RequestHandler;
 
   constructor({
     logger,
     config,
+    authMiddleware,
   }: {
     logger: ILogger;
     config: typeof AppConfig;
+    authMiddleware: RequestHandler;
   }) {
     this.logger = logger;
     this.config = config;
+    this.authMiddleware = authMiddleware;
 
     this.expressApp = express();
 
@@ -26,11 +35,16 @@ export class App {
     this.expressApp.get("/status", (req: Request, res: Response) => {
       res.send("ok");
     });
+
+    this.expressApp.use(this.authMiddleware);
+
+    this.expressApp.get("/protected-route", (req: Request, res: Response) => {
+      res.json({ data: "secret data" });
+    });
   }
 
   public start(): void {
     const port = this.config.port;
-
     this.expressApp.listen(port, () => {
       this.logger.info(`HTTP-сервер успешно запущен на порту ${port}`);
     });
